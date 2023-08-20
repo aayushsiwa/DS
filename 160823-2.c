8c4ccd9affee44d391af4611412e3b45696aeff5
Aayush Siwa <22052177@kiit.ac.in>
2023-08-20 21:22:35 +0530

    updated

--- 160823-2.c
+++ 160823-2.c
@@ -1,32 +1,14 @@
 // wap to find the largest element and its position in an array.
 
 #include<stdio.h>
-
-int array_input(int a[],int n){
-    printf("Enter the elements:-\n");
-    for(int i=0;i<n;i++){
-        printf("%i element- ",i+1);
-        scanf("%d",&a[i]);
-    }
-}
-
-int max_array(int a[],int n){
-    int i,max=0,pos;
-    for(i=0;i<n;i++){
-        if(a[i]>max){
-            max=a[i];
-            pos=i;
-        }else{
-            continue;
-        }
-    }
-    printf("Largest element is %d at %d",max,pos);
-}
+#include "bootstrap.c"
 
 void main(){
-    int a[50],i,n,max=0,maxi;
+    int a[50],n;
     printf("Array Size? ");
     scanf("%d",&n);
     array_input(a,n);
-    max_array(a,n);
+    int* arr=max_array(a,n);
+    printf("Largest number is %d at %d",arr[0],arr[1]);
+    free(arr);
 }
